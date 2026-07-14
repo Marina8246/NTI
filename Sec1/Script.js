9f6d2689f75f308f7461b910a9abee284a8f9676
Marina Shenouda <Marina.Suleiman.2230143@CIT.hnu.edu.eg>
2026-07-14 21:26:58 +0300

    Add files via upload

---
+++ Sec1/Script.js
@@ -0,0 +1,4 @@
+var FirstName = prompt("Enter your first name:");
+var LastName = prompt("Enter your last name:");
+var age = prompt("Enter your age:");
+document.write("Hello " + FirstName + " " + LastName + ", you are " + age + " years old");
